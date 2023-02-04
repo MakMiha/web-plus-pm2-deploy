bdad5ce15c0b626e4bb333487daaf6e03f00f204
MakMiha <mackarowmixail@yandex.ru>
2023-02-05 01:30:32 +0300

    edit config

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -21,7 +21,7 @@ module.exports = {
       repo: DEPLOY_REPO,
       path: DEPLOY_PATH,
       'pre-deploy': `scp ./*.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
-      'post-deploy': 'cd current/backend && npm i && npm run build',
+      'post-deploy': 'cd ~/web-plus-pm2-deploy/current/backend && npm i && npm run build',
     },
   },
 }; 
